e62bb54de2f9d48221a5039a409fe5b6d80fdb0f
kuba <Jakub Troczynski>
2023-05-10 14:35:33 +0200

    commit

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1191,10 +1191,11 @@ if ( ! function_exists( 'auth_redirect' ) ) :
 		if ( $secure && ! is_ssl() && false !== strpos( $_SERVER['REQUEST_URI'], 'wp-admin' ) ) {
 			if ( 0 === strpos( $_SERVER['REQUEST_URI'], 'http' ) ) {
 				wp_redirect( set_url_scheme( $_SERVER['REQUEST_URI'], 'https' ) );
+				var_dump(set_url_scheme( $_SERVER['REQUEST_URI'], 'https' ));
 				exit;
 			} else {
 				var_dump($_SERVER['HTTP_HOST']);
-				var_dump($_SERVER['REQUEST_URI']);
+				
 				//wp_redirect( 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
 				exit;
 			}
