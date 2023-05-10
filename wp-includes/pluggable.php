8d8ea9b63ca02a344f5048cd4fbe3de86f524c1f
kuba <Jakub Troczynski>
2023-05-10 14:38:00 +0200

    commit

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1196,8 +1196,9 @@ if ( ! function_exists( 'auth_redirect' ) ) :
 				exit;
 			} else {
 				//var_dump($_SERVER['HTTP_HOST']);
-				echo 'kuba';
-				//wp_redirect( 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
+				var_dump('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+				//echo 'kuba';
+			//	wp_redirect( 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
 				exit;
 			}
 		}
