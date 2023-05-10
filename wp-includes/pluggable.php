9bdf3bdccec40e15ef44f79a4b57a037fadca32c
kuba <Jakub Troczynski>
2023-05-10 15:08:11 +0200

    commit

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1382,6 +1382,7 @@ if ( ! function_exists( 'wp_redirect' ) ) :
 	 * @return bool False if the redirect was canceled, true otherwise.
 	 */
 	function wp_redirect( $location, $status = 302, $x_redirect_by = 'WordPress' ) {
+		echo 'red';
 		// global $is_IIS;
 
 		// /**
