e1085f21432889863b0c50f7e339708ffc2ab723
kuba <Jakub Troczynski>
2023-05-10 11:19:35 +0200

    commit

---
+++ pull.php
@@ -0,0 +1,15 @@
+<?php
+
+// Use in the “Post-Receive URLs” section of your GitHub repo.
+//hellofff
+
+
+if($_POST['payload']){
+
+
+echo shell_exec('cd /var/www/woo && /usr/bin/git pull origin main 2>&1');
+
+}
+
+//boftffg
+?>
