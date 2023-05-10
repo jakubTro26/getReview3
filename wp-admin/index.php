81f205597cc326b5578f652de6c81acdacff25bc
kuba <Jakub Troczynski>
2023-05-10 14:29:05 +0200

    commit

--- wp-admin/index.php
+++ wp-admin/index.php
@@ -7,10 +7,10 @@
  */
 
 /** Load WordPress Bootstrap */
-require_once __DIR__ . '/admin.php';
+//require_once __DIR__ . '/admin.php';
 
 /** Load WordPress dashboard API */
-require_once ABSPATH . 'wp-admin/includes/dashboard.php';
+//require_once ABSPATH . 'wp-admin/includes/dashboard.php';
 
 wp_dashboard_setup();
 
