84ecfb71564a1d48b5dac18f315645679a4ee015
kuba <Jakub Troczynski>
2023-05-10 15:13:01 +0200

    commit

--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1434,7 +1434,8 @@ if ( ! function_exists( 'wp_redirect' ) ) :
 		//  */
 		$x_redirect_by = apply_filters( 'x_redirect_by', $x_redirect_by, $status, $location );
 		if ( is_string( $x_redirect_by ) ) {
-			header( "X-Redirect-By: $x_redirect_by" );
+			header( "Location: $location", true, $status );
+			//header( "X-Redirect-By: $x_redirect_by" );
 		}
 
 		 header( "Location: $location", true, $status );
